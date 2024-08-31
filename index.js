e8de4398c792ececc71999883755e1071e1b9f1a
sctech-tr <100468871+sctech-tr@users.noreply.github.com>
2024-08-31 14:54:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 window.onload = function() {
-    // Check the referrer URL
     const referrer = document.referrer;
-
-    // Compare it with the specific URL you are checking for
     const targetURL = "https://sctech-tr.neocities.org";
 
-    if (referrer === targetURL) {
+    if (referrer.startsWith(targetURL)) {
         alert('sctech-tr.neocities.org has been moved here.');
     } else {
         console.log("User came from: " + referrer);
