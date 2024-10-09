26be73bdbf546eee864e68114c5ea6c0d6b5a6e2
sctech-tr <sctechwastaken@proton.me>
2024-10-09 19:42:20 +0300

    change mail again

--- update_json.py
+++ update_json.py
@@ -51,7 +51,7 @@ def update_json():
         # If the file doesn't exist, create a default structure
         data = {
             "name": "if you're seeing this, the workflow is broken.",
-            "meal": "please contact gq2ic3wbe@mozmail.com",
+            "meal": "please contact sctech-tr@googlegroups.com",
             "quote": "No quote available"
         }
 
