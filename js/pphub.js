70d71b9fe1f36ebeea205a572171c0af2a3a54f3
sctech-tr <gamerselimiko@gmail.com>
2024-11-25 18:38:25 +0300

    add ublock origin button + js -> scripts + delete unused js files

--- js/pphub.js
+++
@@ -1,8 +0,0 @@
-function redirectToPage() {
-    const pageUrl = document.getElementById("pageUrl").value;
-    if (pageUrl) {
-      window.location.href = "users/" + pageUrl + ".html";
-    } else {
-      alert("please enter a valid username");
-    }
-}
